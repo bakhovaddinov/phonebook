ae70e6ea8fee505b46d7c13157c3b22db71e8ba7
bakhovaddinov <bakhovaddinov@gmail.com>
2020-11-22 15:33:39 +0300

    Updated main and record Added output functionality

--- phonebook/__main__.py
+++ phonebook/__main__.py
@@ -1,14 +1,32 @@
 from phonebook.entities.record import Record
 from phonebook.store import Store
 from datetime import date
+import sqlite3
 
-command = input('Do you wish to add a record? [y/n]')
+command = ''
+while command != 'Exit':
+    command = input('''Hello! 
+    what do you wish to do?
+    available options: 
+    [1] Add a record
+    [2] Deleate a record
+    [3] Show the whole phonebook
+    [4] Edit a record
+    to exit print "Exit"\n ''')
 
-if command == 'y':
-    first_name = input('Enter your first name: ')
-    last_name = input('Enter your last name: ')
-    phone = input('Enter your phone number: ')
-    birth_date = date.fromisoformat(input('Enter your birth date: '))
-    store = Store()
-    store.add_record(Record(first_name, last_name, phone, birth_date))
-    store.close()
+    if command == '1':
+        first_name = input('Enter your first name: ')
+        last_name = input('Enter your last name: ')
+        phone = input('Enter your phone number: ')
+        birth_date = date.fromisoformat(input('Enter your birth date: '))
+        store = Store()
+        store.add_record(Record(first_name, last_name, phone, birth_date))
+        store.close()
+    elif command == '2':
+        first_name, last_name = input('Enter first name and last name: ').split(' ')
+        store = Store()
+        store.remove_record(first_name, last_name)
+    elif command == '3':
+        store = Store()
+        store.output()
+
--- phonebook/store.py
+++ phonebook/store.py
@@ -2,9 +2,11 @@ import sqlite3
 from phonebook.entities.record import Record
 
 class Store:
+    
     def __init__(self):
         self.connection = sqlite3.connect("phonebook.db")
-        self.connection.execute("""create table if not exists Record (
+        self.base = self.connection.cursor()
+        self.base.execute("""create table if not exists Record (
             first_name text,
             last_name text,
             phone_number text,
@@ -13,11 +15,15 @@ class Store:
         self.connection.commit()
 
     def add_record(self, record: Record):
-        self.connection.execute('insert into Record values (?, ?, ?, ?)', record.to_tuple())
+        self.base.execute('insert into Record values (?, ?, ?, ?)', record.to_tuple())
         self.connection.commit()
 
-    def remove_record(self, f_name, l_name):
-        self.connection.execute('delete from Record where first_name = f_name and last_name = l_name')
+    def remove_record(self, first_name, last_name):
+        self.base.execute('delete from Record where first_name = first_name and last_name = last_name')
+
+    def output(self):
+        self.base.execute('select * from Record')
+        print(self.base.fetchall())
 
     def close(self):
         self.connection.close()
