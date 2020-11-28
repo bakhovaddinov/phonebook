b469eead82ff52bbdd78641194f9a7ae7a9a8f49
bakhovaddinov <bakhovaddinov@gmail.com>
2020-11-28 21:48:30 +0300

    Regrouped files and funcs and implemented removing

--- phonebook/__main__.py
+++ phonebook/__main__.py
@@ -1,48 +1,14 @@
-from phonebook.design import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication
 import sys
-from phonebook.entities.record import Record
-from phonebook.store import Store
-from datetime import date
-import sqlite3
-from phonebook.input_window import DialogWindow
-
-class MyWidget(QMainWindow, Ui_MainWindow):
-    
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.store = Store()
-        self.refresh()
-        self.add_btn.clicked.connect(self.adding)
-        self.rmv_btn.clicked.connect(self.remove)
-
-    def adding(self):
-        window = DialogWindow()
-        window.show()
-        window.exec()
-        self.refresh()
-    
-    def refresh(self):
-        self.phonebook.clear()
-        for element in self.store.output():
-            self.phonebook.addItem(f'{element[0]} {element[1]}  —  {element[2]}  —  {element[3]}')
-    
-    def remove(self):
-        self.store = Store()
-        data = self.phonebook.currentItem().text().split('  —  ')
-        first_name, last_name = data[0].split()
-        self.store.remove_record(first_name, last_name, data[1])
-        self.refresh()
-        
-        
-        
+from PyQt5.QtWidgets import QApplication
+from phonebook.ui.mainwidget import MainWidget
 
 
+def main():
+    app = QApplication(sys.argv)
+    ex = MainWidget()
+    ex.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    main()
--- phonebook/store.py
+++ phonebook/store.py
@@ -1,13 +1,13 @@
 import sqlite3
 from phonebook.entities.record import Record
 
+
 class Store:
-    
     def __init__(self):
         self.connection = sqlite3.connect("phonebook.db")
         self.base = self.connection.cursor()
         self.base.execute("""create table if not exists Record (
-            first_name text,
+            first_name text UNIQUE,
             last_name text,
             phone_number text,
             birth_date date
@@ -15,17 +15,18 @@ class Store:
         self.connection.commit()
 
     def add_record(self, record: Record):
-        self.base.execute('insert into Record values (?, ?, ?, ?)', record.to_tuple())
+        self.base.execute(
+            'insert into Record values (?, ?, ?, ?)', record.to_tuple())
         self.connection.commit()
 
     def remove_record(self, first_name, last_name, phone_number):
-        self.base.execute('delete from Record where first_name = first_name and last_name = last_name and phone_number = phone_number')
+        query = f"delete from Record where first_name = '{first_name}' and last_name = '{last_name}' and phone_number = '{phone_number}'"
+        self.base.execute(query)
         self.connection.commit()
 
-    def output(self):
-        self.base.execute('select * from Record')   
+    def get_records(self):
+        self.base.execute('select * from Record')
         return self.base.fetchall()
 
     def close(self):
         self.connection.close()
-
